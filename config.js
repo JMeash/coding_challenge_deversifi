ea76a1e54ee5007de366591f5ca1ca3aca598a25
JMeash <jmmedranomm@gmail.com>
2020-11-26 16:09:14 +0000

    Add the execution of bids and asks

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const BigNumber = require('bignumber.js');
+
 const config = {
     diversifi: {
         url: process.env.DIVERSIFI_API_PATH || 'https://api.stg.deversifi.com/bfx/v2/book/',
@@ -5,8 +7,8 @@ const config = {
         precision: process.env.DIVERSIFI_PRECISION || 'R0',
     },
     starting_balance: {
-        eth: 10.0,
-        usd: 2000.0,
+        eth: new BigNumber(10),
+        usd: new BigNumber(2000),
     },
     timers: {
         bid_refresh: 5,
