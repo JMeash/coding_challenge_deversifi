46773e6e936079e45f822aed762d7c4780ba29fc
JMeash <jmmedranomm@gmail.com>
2020-11-26 12:04:47 +0000

    Add master balance service

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const orders = require('./lib/services/orders');
+const mbService = require('./lib/services/masterBalance');
 const config = require('./config');
 const cron = require('node-cron');
 
 cron.schedule(`*/${config.timers.master_balance} * * * * *`, () => {
-    console.log('running a task every 30s');
+    const masterBalance = mbService.get();
+    console.log(`CURRENT BALANCE - ETH:${masterBalance.eth} AND USD:${masterBalance.usd}`);
 });
 
 cron.schedule(`*/${config.timers.bid_refresh} * * * * *`, () => {
